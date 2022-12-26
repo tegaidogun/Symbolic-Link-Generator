7607765a9e73ccedb0fd6eae629ce10ec3177eaa
Oghenetega Idogun <otegaid@gmail.com>
2022-12-26 18:57:43 -0500

    First Change

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Symbolic-Link-Generator',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/tegaidogun/Symbolic-Link-Generator',
+    license='MIT License',
+    author='Oghenetega Eric Idgoun',
+    author_email='otegaid@gmail.com',
+    description='This is a simple Symbolic Link Generator written in python using Pycharm (Alpha Version).'
+)
